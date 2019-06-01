3f28752d9cf9ae1c7942d79aa3c1a9ab7d258c55
Roman1137 <poma.borodavka@gmail.com>
2019-06-01 12:35:33 +0300

    formatted do-while condition

--- TrainTicketWatcher/Program.cs
+++ TrainTicketWatcher/Program.cs
@@ -60,7 +60,9 @@ namespace TrainTicketWatcher
 
                         response = new ApliClient().PostRequest("https://booking.uz.gov.ua/ru/train_search/", tripDataToUse).Result;
 
-                    } while (response.IsServiceUnAvailable || response.IsUnsuccessfulResponse || !response.IsFreePlacePresentByTypes(tripDataToUse));
+                    } while (response.IsServiceUnAvailable 
+                             || response.IsUnsuccessfulResponse ||
+                             !response.IsFreePlacePresentByTypes(tripDataToUse));
                 }
                 catch (Exception e)
                 {
